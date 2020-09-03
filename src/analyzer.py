5027d34e57563c65ef2b799787f8fc2bb610e20c
Mattia Milani <mattia.milani@studenti.unitn.it>
2020-09-03 09:58:32 +0200

    now is possible to load and save csv in pickle format using the -pi command in the analyzer

--- src/analyzer.py
+++ src/analyzer.py
@@ -21,6 +21,7 @@ import sys
 import timeit
 from glob import glob
 import pandas as pd
+import pickle
 
 sys.path.insert(1, 'util')
 from analysis import SingleFileAnalysis
@@ -55,6 +56,11 @@ parser.add_argument("-v", "--verbose", dest="verbose", default=False,
                           on standard output ")
 parser.add_argument("-p", "--progress", dest="progress", default=True,
                     action='store_false', help="Display a progressbar on stdout")
+parser.add_argument("-pi", "--pickle", dest="pickle", default=False,
+                    action='store_true', help="if possible use saved pickles \
+                            from the input folder instead of load again the  \
+                            entire dataset, at the end it will save dataframes \
+                            in pickle format in the output folder")
 
 if __name__ == "__main__":
     # Parse the arguments
@@ -76,115 +82,137 @@ if __name__ == "__main__":
     states_route = None
     i = 0
 
-    pbar = tqdm(options.inputFile) if options.progress else options.inputFile
-    for inputFile_path in pbar:
-        if options.progress:
-            pbar.set_description("Processing {}".format(inputFile_path))
-        else:
-            print("Processing {}".format(inputFile_path))
-        # Check that the input file exists
-        if not os.path.isfile(inputFile_path):
-            # doesn't exist
-            print("Input file {} not found".format(inputFile_path))
-            raise FileNotFoundError
-    
-        # Get the dataframe rep of the input file
-        if options.time:
-            starttime = timeit.default_timer()
-            init_time= timeit.default_timer()
+    # Check if it is possible to load pickles files
+    if options.pickle:
+        # Check that all pickle files exists
+        if os.path.isfile(outputFile_path + "_states.pkl") and \
+           os.path.isfile(outputFile_path + "_transitions.pkl") and \
+           os.path.isfile(outputFile_path + "_route_id.pkl") and \
+           os.path.isfile(outputFile_path + "_states_id.pkl"):
+            # Load pickles files
+            states_df = pickle.load(open(outputFile_path + "_states.pkl", "rb"))
+            transitions_df = pickle.load(open(outputFile_path + "_transitions.pkl", "rb"))
+            route_to_id = pickle.load(open(outputFile_path + "_route_id.pkl", "rb"))
+            states_route = pickle.load(open(outputFile_path + "_states_id.pkl", "rb"))
 
-        sf = SingleFileAnalysis(inputFile_path, route_df=route_to_id,
-                                states_routes=states_route)
-
-        if options.time:
-            print("The init time has been:", timeit.default_timer() - init_time)
-        if options.verbose:
-            print("Initialization done, csv file loaded")
-
-        if options.time:
-            select_node_time = timeit.default_timer()
-
-        # Node selection
-        sf.df = sf.selectNode(node)
-
-        if options.time:
-            print("The select node time has been:", timeit.default_timer() - select_node_time)
-        if options.verbose:
-            print("Node selection done, dataframe updated")
-
-        if options.time:
-            keep_events_time = timeit.default_timer()
-
-        # Keep only fsm reliable events
-        sf.df = sf.keep_only_fsm_events()
-
-        if options.time:
-            print("The keep fsm events time has been :", timeit.default_timer() - keep_events_time)
-        if options.verbose:
-            print("Node fsm reliable events parsing done, dataframe updated")
-
-        if options.time:
-            evaluate_time= timeit.default_timer()
-
-        sf.evaluate_fsm()
+    # If states is not none means that pickles has been loaded and it is not 
+    # necessary to parse the files
+    if isinstance(states_df, NoneType):
+        pbar = tqdm(options.inputFile) if options.progress else options.inputFile
+        for inputFile_path in pbar:
+            if options.progress:
+                pbar.set_description("Processing {}".format(inputFile_path))
+            else:
+                print("Processing {}".format(inputFile_path))
+            # Check that the input file exists
+            if not os.path.isfile(inputFile_path):
+                # doesn't exist
+                print("Input file {} not found".format(inputFile_path))
+                raise FileNotFoundError
         
-        if options.time:
-            print("The evaluate time has been:", timeit.default_timer() - evaluate_time)
-            print("The total time required by the evaluation has been:", timeit.default_timer() - starttime)
-        if options.verbose:
-            print("Evaluation of fsm components done")
-        
-        sr_df = sf.get_states_as_df()
-        if isinstance(states_df, NoneType):
-            states_df = sr_df
-            states_df[str(i)] = sr_df['counter']
-        else:
-            sr_df[str(i)] = sr_df['counter']
-            sr_df = sr_df.drop(['counter'], axis=1)
-            sr_df_states = sr_df.drop([str(i)], axis=1)
-            states_df = pd.concat([states_df, sr_df_states])
-            states_df = states_df[~states_df.index.duplicated(keep='first')]
-            states_df = states_df.fillna(0)
-            sr_df = sr_df.drop(['state'], axis=1)
-            states_df = pd.concat([states_df, sr_df], axis=1)
-            states_df = states_df.fillna(0)
-            states_df['counter'] = states_df['counter'] + states_df[str(i)]
+            # Get the dataframe rep of the input file
+            if options.time:
+                starttime = timeit.default_timer()
+                init_time= timeit.default_timer()
 
-        states_df['counter'] = states_df['counter'].astype(int)
-        for j in range(0,i):
-            states_df[str(j)] = states_df[str(j)].astype(int)
+            sf = SingleFileAnalysis(inputFile_path, route_df=route_to_id,
+                                    states_routes=states_route)
 
-        tr_df = sf.get_transitions_as_df()
-        if isinstance(transitions_df, NoneType):
-            transitions_df = tr_df
-            transitions_df[str(i)] = tr_df['counter']
-        else:
-            tr_df[str(i)] = tr_df['counter']
-            tr_df = tr_df.drop(['counter'], axis=1)
-            tr_df_states = tr_df.drop([str(i)], axis=1)
-            transitions_df = pd.concat([transitions_df, tr_df_states])
-            transitions_df = transitions_df[~transitions_df.index.duplicated(keep='first')]
-            transitions_df = transitions_df.fillna(0)
-            tr_df = tr_df.drop(['start_node', 'end_node', 'cause', 'response'], axis=1)
-            transitions_df = pd.concat([transitions_df, tr_df], axis=1)
-            transitions_df = transitions_df.fillna(0)
-            transitions_df['counter'] = transitions_df['counter'] + transitions_df[str(i)]
-        print(transitions_df)
-        for j in range(0,i):
-            transitions_df[str(j)] = transitions_df[str(j)].astype(int)
-        transitions_df['counter'] = transitions_df['counter'].astype(int)
+            if options.time:
+                print("The init time has been:", timeit.default_timer() - init_time)
+            if options.verbose:
+                print("Initialization done, csv file loaded")
 
-        route_to_id = sf.get_route_df()
-        states_route = sf.get_states_route_df()
+            if options.time:
+                select_node_time = timeit.default_timer()
 
-        del sf
-        i += 1
+            # Node selection
+            sf.df = sf.selectNode(node)
+
+            if options.time:
+                print("The select node time has been:", timeit.default_timer() - select_node_time)
+            if options.verbose:
+                print("Node selection done, dataframe updated")
+
+            if options.time:
+                keep_events_time = timeit.default_timer()
+
+            # Keep only fsm reliable events
+            sf.df = sf.keep_only_fsm_events()
+
+            if options.time:
+                print("The keep fsm events time has been :", timeit.default_timer() - keep_events_time)
+            if options.verbose:
+                print("Node fsm reliable events parsing done, dataframe updated")
+
+            if options.time:
+                evaluate_time= timeit.default_timer()
+
+            sf.evaluate_fsm()
+            
+            if options.time:
+                print("The evaluate time has been:", timeit.default_timer() - evaluate_time)
+                print("The total time required by the evaluation has been:", timeit.default_timer() - starttime)
+            if options.verbose:
+                print("Evaluation of fsm components done")
+            
+            sr_df = sf.get_states_as_df()
+            if isinstance(states_df, NoneType):
+                states_df = sr_df
+                states_df[str(i)] = sr_df['counter']
+            else:
+                sr_df[str(i)] = sr_df['counter']
+                sr_df = sr_df.drop(['counter'], axis=1)
+                sr_df_states = sr_df.drop([str(i)], axis=1)
+                states_df = pd.concat([states_df, sr_df_states])
+                states_df = states_df[~states_df.index.duplicated(keep='first')]
+                states_df = states_df.fillna(0)
+                sr_df = sr_df.drop(['state'], axis=1)
+                states_df = pd.concat([states_df, sr_df], axis=1)
+                states_df = states_df.fillna(0)
+                states_df['counter'] = states_df['counter'] + states_df[str(i)]
+
+            states_df['counter'] = states_df['counter'].astype(int)
+            for j in range(0,i):
+                states_df[str(j)] = states_df[str(j)].astype(int)
+
+            tr_df = sf.get_transitions_as_df()
+            if isinstance(transitions_df, NoneType):
+                transitions_df = tr_df
+                transitions_df[str(i)] = tr_df['counter']
+            else:
+                tr_df[str(i)] = tr_df['counter']
+                tr_df = tr_df.drop(['counter'], axis=1)
+                tr_df_states = tr_df.drop([str(i)], axis=1)
+                transitions_df = pd.concat([transitions_df, tr_df_states])
+                transitions_df = transitions_df[~transitions_df.index.duplicated(keep='first')]
+                transitions_df = transitions_df.fillna(0)
+                tr_df = tr_df.drop(['start_node', 'end_node', 'cause', 'response'], axis=1)
+                transitions_df = pd.concat([transitions_df, tr_df], axis=1)
+                transitions_df = transitions_df.fillna(0)
+                transitions_df['counter'] = transitions_df['counter'] + transitions_df[str(i)]
+            for j in range(0,i):
+                transitions_df[str(j)] = transitions_df[str(j)].astype(int)
+            transitions_df['counter'] = transitions_df['counter'].astype(int)
+
+            route_to_id = sf.get_route_df()
+            states_route = sf.get_states_route_df()
+
+            del sf
+            i += 1
 
     # Save results
     SingleFileAnalysis.dump_df(outputFile_path + "_states.csv", states_df)
     SingleFileAnalysis.dump_df(outputFile_path + "_transitions.csv", transitions_df)
     SingleFileAnalysis.dump_df(outputFile_path + "_route_id.csv", route_to_id)
     SingleFileAnalysis.dump_df(outputFile_path + "_states_id.csv", states_route)
+
+    # Save results as pickle
+    if options.pickle:
+        pickle.dump(states_df, open(outputFile_path + "_states.pkl", "wb"))
+        pickle.dump(transitions_df, open(outputFile_path + "_transitions.pkl", "wb"))
+        pickle.dump(route_to_id, open(outputFile_path + "_route_id.pkl", "wb"))
+        pickle.dump(states_route, open(outputFile_path + "_states_id.pkl", "wb"))
     
     #Generate the graph
     plt = Plotter(states_df, transitions_df, route_to_id)
--- src/install.sh
+++ src/install.sh
@@ -6,12 +6,16 @@
 
 sudo apt-get install python-virtualenv prallel
 
+rm -rf env_test
+
 mkdir env_test
 virtualenv env_test
 source env_test/bin/activate
 env_test/bin/pip3 install -r requirements.txt 
+env_test/bin/pip3 freeze |sed -ne 's/==.*//p' |xargs env_test/bin/pip3 install -U --
 deactivate
 
+echo " "
 echo "Virtual environment with all the libraries required created"
 echo "Please use 'source env_test/bin/activate' before using the software"
 echo "use 'deactivate' to exit from the virtual environment"
