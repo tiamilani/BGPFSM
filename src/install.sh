0e2c071ba87c5640685694400eb6e53417630265
Mattia Milani <mattia.milani@studenti.unitn.it>
2020-09-03 11:01:53 +0200

    fix install.sh

--- src/install.sh
+++ src/install.sh
@@ -4,12 +4,12 @@
 # and analyze them
 #
 
-sudo apt-get install python-virtualenv parallel
+sudo apt-get install python-virtualenv parallel graphviz
 
 rm -rf env_test
 
 mkdir env_test
-virtualenv env_test
+virtualenv --python=$(which python3) env_test
 source env_test/bin/activate
 env_test/bin/pip3 install -r requirements.txt 
 env_test/bin/pip3 freeze |sed -ne 's/==.*//p' |xargs env_test/bin/pip3 install -U --
